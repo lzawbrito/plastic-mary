334d06147e04f934013a31a210cdb8a61def9a30
Lucas Brito <luzabri7@gmail.com>
2024-04-17 09:11:25 -0400

    editing deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-git subtree pull --prefix dist origin gh-pages
-git subtree push --prefix dist origin gh-pages
+# git subtree pull --prefix dist origin gh-pages
+git push origin `git subtree split --prefix dist master`:gh-pages --force

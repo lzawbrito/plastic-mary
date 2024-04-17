a309fc68f45cedf38a587368555cc3dff4a8db54
Lucas Brito <luzabri7@gmail.com>
2024-04-17 09:41:50 -0400

    tore everything down new things up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
 <template>
-  <main>PLASTIC MARY</main>
+  <main>
+    plastic mary is band of five members.
+    <a
+      target="_blank"
+      href="https://open.spotify.com/artist/0TOasfVXhPtb4wKoBJLL4E?si=sSsAafykRA67Rk9Q8BhJpg"
+      >spotify</a
+    >,
+    <a target="_blank" href="https://www.instagram.com/plasticmaryband/"> instagram </a>
+  </main>
 </template>
